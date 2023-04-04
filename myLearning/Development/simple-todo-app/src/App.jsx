36da60b5b505cc1abfc16d96f42b43206b8c7a3c
Mourice-Adero <aderomourice7@gmail.com>
2023-04-04 22:31:57 +0300

    start: react todo app

---
+++ myLearning/Development/simple-todo-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+const App = () => {
+  return (
+    <React.Fragment>
+      <div>
+        <h1>Level 1</h1>
+      </div>
+      <div>
+        <h1>Level 2</h1>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default App;
