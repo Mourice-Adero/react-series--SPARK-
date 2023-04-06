f8491174f8f289a971be9783361006b6a28dc225
Mourice-Adero <aderomourice7@gmail.com>
2023-04-07 00:31:17 +0300

    css: card component

--- myLearning/Development/simple-todo-app/src/App.jsx
+++ myLearning/Development/simple-todo-app/src/App.jsx
@@ -1,14 +1,31 @@
 import React from "react";
+import "./App.css";
+// const imageLink = 'https://v1.tailwindcss.com/img/card-top.jpg'
 const App = () => {
   return (
-    <React.Fragment>
-      <div>
-        <h1>Level 1</h1>
+    <div className="page-wrapper">
+      <div className="card-component">
+        <img
+          src="https://v1.tailwindcss.com/img/card-top.jpg"
+          alt=""
+          className="card-image"
+        />
+        <div className="card-content">
+          <h1>The Coldest Sunset</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam
+            impedit accusantium sequi consequatur? Totam, distinctio accusantium
+            possimus vitae, molestiae laudantium iure natus odio dignissimos,
+            autem voluptatem sequi explicabo porro tempore.
+          </p>
+          <div className="tags">
+            <span className="tag">#photography</span>
+            <span className="tag">#travel</span>
+            <span className="tag">#winter</span>
+          </div>
+        </div>
       </div>
-      <div>
-        <h1>Level 2</h1>
-      </div>
-    </React.Fragment>
+    </div>
   );
 };
 
